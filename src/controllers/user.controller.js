6ca0e4bb263111a400447bd774bb6828ec5419c7
najaf.shafi <najafshafee5@gmail.com>
2024-10-07 23:53:43 +0500

    Add validation for required fields in user registration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,15 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
 
 const registerUser = asyncHandler(async (req, res) => {
+
+    const { email, password, username, fullName, avatar, coverImage, } = req.body;
+
+    if (!email || !password || !username || !fullName || !avatar || !coverImage) {
+        return res.status(400).json({
+            message: 'Please fill all fields',
+        });
+    }
+
     res.status(200).json({
         message: 'User Registered Successfully',
     });
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middlewares.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+    upload.fields([
+        { name: "avatar", maxCount: 1 },
+        {
+            name: "coverImage", maxCount: 1,
+        },]),
 
+    registerUser);
 
-router.route("/h").get((req, res) => {
-    res.status(200).json({
-        message: "H1"
-    })
-})
 
 export default router;
